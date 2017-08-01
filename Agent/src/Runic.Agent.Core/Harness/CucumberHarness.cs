6f6d1c812f178a04d3293876c288ed17e0ea3cdf
joshuagenders <joshuagenders@hotmail.com>
2017-08-01 22:11:59 +0100

    fixing naming and target framework

---
+++ Agent/src/Runic.Agent.Core/Harness/CucumberHarness.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Runic.Cucumber;
+using Runic.Agent.Core.AssemblyManagement;
+
+namespace Runic.Agent.Core.Harness
+{
+    public class CucumberHarness
+    {
+        private readonly IPluginManager _pluginManager;
+        public CucumberHarness(IPluginManager pluginManager)
+        {
+            _pluginManager = pluginManager;
+        }
+        public async Task ExecuteTestAsync(string assemblyName, string document)
+        {
+            var test = new CucumberTest(_pluginManager.LoadPlugin(assemblyName));
+        }
+    }
+}
