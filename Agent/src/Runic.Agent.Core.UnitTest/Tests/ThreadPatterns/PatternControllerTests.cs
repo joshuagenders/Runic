bdfd7d39a78752a708d26a835941342d77d7382c
joshuagenders <joshuagenders@hotmail.com>
2017-09-03 22:42:18 +0100

    adding test

--- Agent/src/Runic.Agent.Core.UnitTest/Tests/ThreadPatterns/PatternControllerTests.cs
+++ Agent/src/Runic.Agent.Core.UnitTest/Tests/ThreadPatterns/PatternControllerTests.cs
@@ -1,5 +1,14 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Runic.Agent.Core.Services;
+using Runic.Agent.Core.ThreadManagement;
+using Runic.Agent.Core.ThreadPatterns;
+using Runic.Agent.Core.UnitTest.TestUtility;
+using Runic.Framework.Models;
 using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Runic.Agent.Core.UnitTest.Tests.ThreadPatterns
 {
@@ -7,9 +16,32 @@ namespace Runic.Agent.Core.UnitTest.Tests.ThreadPatterns
     public class PatternControllerTests
     {
         [TestMethod]
-        public void WhenRunningPattern_ThenThreadLevelsAreSet()
+        public async Task WhenRunningPattern_ThenThreadLevelsAreSet()
         {
-            throw new NotImplementedException();
+            var datetimeService = new Mock<IDatetimeService>();
+            var startTime = new DateTime();
+            datetimeService.Setup(s => s.Now).Returns(startTime);
+            var threadManager = new Mock<IThreadManager>();
+            var patternController = new PatternController(datetimeService.Object, threadManager.Object);
+            var flow = TestData.GetTestFlowSingleStep;
+            var pattern = new Mock<IThreadPattern>();
+
+            var cts = new CancellationTokenSource();
+            cts.CancelAfter(1200);
+
+            patternController.StartThreadPattern("id", flow, pattern.Object, cts.Token);
+            Assert.IsTrue(patternController.GetRunningThreadPatterns().Any(p => p.Item1 == "id"));
+            Assert.IsTrue(patternController.GetRunningFlows().Any(p => p.Item1 == "id"));
+
+            pattern.Setup(p => p.GetCurrentThreadLevel(It.IsAny<DateTime>())).Returns(20);
+            var controllerTask = patternController.Run(cts.Token);
+
+            threadManager.Verify(t => t.SetThreadLevelAsync(It.Is<SetThreadLevelRequest>(r => r.ThreadLevel == 20), It.IsAny<CancellationToken>()));
+
+            pattern.Setup(p => p.GetCurrentThreadLevel(It.IsAny<DateTime>())).Returns(0);
+            await controllerTask;
+            Assert.IsFalse(patternController.GetRunningThreadPatterns().Any(p => p.Item1 == "id"));
+            Assert.IsFalse(patternController.GetRunningFlows().Any(p => p.Item1 == "id"));
         }
 
         [TestMethod]
--- Agent/src/Runic.Agent.Core/ThreadPatterns/PatternController.cs
+++ Agent/src/Runic.Agent.Core/ThreadPatterns/PatternController.cs
@@ -3,6 +3,7 @@ using Runic.Agent.Core.ThreadPatterns;
 using Runic.Framework.Models;
 using System;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -25,6 +26,16 @@ namespace Runic.Agent.Core.Services
             _runningPatterns = new ConcurrentDictionary<string, Tuple<DateTime, IThreadPattern>>();
         }
         
+        public List<Tuple<string,Flow>> GetRunningFlows()
+        {
+            return _runningFlows.Select(f => Tuple.Create(f.Key, f.Value)).ToList();
+        }
+
+        public List<Tuple<string, IThreadPattern>> GetRunningThreadPatterns()
+        {
+            return _runningPatterns.Select(f => Tuple.Create(f.Key, f.Value.Item2)).ToList();
+        }
+
         public void StartThreadPattern(string id, Flow flow, IThreadPattern pattern, CancellationToken ctx)
         {
             _runningFlows.TryAdd(id, flow);
@@ -86,9 +97,19 @@ namespace Runic.Agent.Core.Services
                         ThreadLevel = threadCount
                     };
                     await _threadManager.SetThreadLevelAsync(request);
+                    if (threadCount == 0)
+                    {
+                        await Stop(pattern.Key, ctx);   
+                    }
                 }
                 await _datetimeService.WaitMilliseconds(PollingIntervalMilliseconds);
+                if (_runningPatterns.Count == 0)
+                {
+                    break;
+                }
             }
+            _runningPatterns.Clear();
+            _runningFlows.Clear();
         }
     }
 }
