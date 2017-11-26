8a7e5410c455d5d91fa6cdb15e8532bb3fdc4fde
joshuagenders <joshuagenders@hotmail.com>
2017-11-26 17:55:30 +0000

    gtbg

--- Agent/src/Runic.Agent.Core.UnitTest/Tests/MethodParameterServiceTests.cs
+++ Agent/src/Runic.Agent.Core.UnitTest/Tests/MethodParameterServiceTests.cs
@@ -1,10 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using FluentAssertions;
+using Runic.Agent.Core.Services;
+using System;
+using System.Linq;
+using System.Reflection;
+using Xunit;
 
 namespace Runic.Agent.Core.UnitTest.Tests
 {
-    class MethodParameterServiceTests
+    public class MethodParameterServiceTests
     {
+        [Fact]
+        public void WhenNoInputs_ThenEmptyArrayReturned()
+        {
+            var mps = new MethodParameterService();
+            var parms = mps.GetParams(new string [] { }, GetType().GetTypeInfo().GetMethod("NoInputs", new Type [] { }));
+            parms.Should().BeEmpty();
+        }
+        [Fact]
+        public void WhenSingleStringInput_ThenNullReturned()
+        {
+            var mps = new MethodParameterService();
+            var parms = mps.GetParams(new string[] { }, GetType().GetTypeInfo().GetMethod("SingleStringInput", new Type[] { typeof(string) }));
+            parms.Count().Should().Be(1);
+            parms.Single().Should().BeNull();
+        }
+        [Fact]
+        public void WhenSingleStringWithDefaulInput_ThenDefaultReturned()
+        {
+            var mps = new MethodParameterService();
+            var parms = mps.GetParams(new string[] { }, GetType().GetTypeInfo().GetMethod("DefaultStringInput", new Type[] { typeof(string) }));
+            parms.Count().Should().Be(1);
+            parms.Single().Should().Be("mydefault");
+        }
+        public void NoInputs()
+        {
+
+        }
+        public void SingleStringInput(string input)
+        {
+
+        }
+        public void DefaultStringInput(string input = "mydefault")
+        {
+
+        }
     }
 }
--- Agent/src/Runic.Agent.Core/Services/MethodParameterService.cs
+++ Agent/src/Runic.Agent.Core/Services/MethodParameterService.cs
@@ -4,38 +4,46 @@ using System.Reflection;
 
 namespace Runic.Agent.Core.Services
 {
-    internal class MethodParameterService
+    public class MethodParameterService
     {
         public object[] GetParams(string[] positionalParameters, MethodInfo methodInfo)
         {
-            if (positionalParameters == null || !positionalParameters.Any())
-            {
-                return new object[] { };
-            }
+            //if (!methodInfo.GetParameters().Any())
+            //{
+            //    return new object[] { };
+            //}
             return GetMapMethodParameters(positionalParameters, methodInfo);
         }
 
         private object[] GetMapMethodParameters(object[] positionalParameters, MethodBase methodInfo)
         {
             var p = methodInfo.GetParameters();
+            if (p == null)
+            {
+                return new object[] { };
+            }
             var methodParams = new object[p.Length];
 
-            //add positional params
-            for (var i = 0; i < positionalParameters.Length; i++)
+            int mappedParamCount = positionalParameters == null ? 0 : positionalParameters.Length;
+            if (positionalParameters != null)
             {
-                if (i < p.Length)
+                //add positional params
+                for (var i = 0; i < mappedParamCount; i++)
                 {
-                    //change type to match
-                    methodParams[i] = Convert.ChangeType(positionalParameters[i], p[i].ParameterType); 
+                    if (i < p.Length)
+                    {
+                        //change type to match
+                        methodParams[i] = Convert.ChangeType(positionalParameters[i], p[i].ParameterType);
+                    }
                 }
             }
             //add defaults for remaining params
-            for (var i = positionalParameters.Length; i < p.Length; i++)
+            for (var i = mappedParamCount; i < p.Length; i++)
             {
                 if (p[i].HasDefaultValue)
                     methodParams[i] = p[i].DefaultValue;
                 else if (!p[i].IsOptional)
-                    methodParams[i] = p[i].ParameterType.IsByRef ? null : Activator.CreateInstance(p[i].ParameterType);
+                    methodParams[i] = p[i].ParameterType.IsValueType ? Activator.CreateInstance(p[i].ParameterType) : null;
             }
             return methodParams;
         }
