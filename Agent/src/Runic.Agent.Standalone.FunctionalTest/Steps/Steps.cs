1f0ba1baaff8cb7094ccb6bb3dd46bdc20b83381
joshuagenders <joshuagenders@hotmail.com>
2017-11-26 22:16:52 +0000

    add functional test project

---
+++ Agent/src/Runic.Agent.Standalone.FunctionalTest/Steps/Steps.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+
+namespace Runic.Agent.FunctionalTest.Steps
+{
+    public class Steps 
+    {
+        public Steps GivenIHaveAJourneyFor(string journeyKey)
+        {
+            return this;
+        }
+
+        public Steps GivenIHaveAFrequencyPattern(string patternType)
+        {
+            return this;
+        }
+
+        public Steps GivenTheJourneyLastsForSeconds(int journeyLengthSeconds)
+        {
+            return this;
+        }
+
+        public Steps GivenTheFrequencyIsJourneysPerMinute(int journeysPerMinute)
+        {
+            return this;
+        }
+
+        public Steps WhenIEmbarkOnTheJourney()
+        {
+            return this;
+        }
+
+        public Steps ThenThePageIsReturnedAtleastTimes(string pageName, int times)
+        {
+            return this;
+        }
+    }
+}
---
+++ Agent/src/Runic.Agent.Standalone.FunctionalTest/TestUtility/TestEnvironmentBuilder.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Runic.Agent.FunctionalTest.TestUtility
+{
+    public class TestEnvironmentBuilder
+    {
+    }
+}
